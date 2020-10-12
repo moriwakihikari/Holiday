58bde770db05221b50555213504f423c95f129d0
moriwakihikari <daiichidaimandaikichi.gi@gmail.com>
2020-10-12 09:00:40 +0000

    ルート、blade用意

--- app/Http/Controllers/HolidayAppController.php
+++ app/Http/Controllers/HolidayAppController.php
@@ -7,9 +7,41 @@ use Illuminate\Http\Request;
 class HolidayAppController extends Controller
 {
     
+    //admin共通
+    public function index()
+    {
+        return view('holiday.index');
+    }
+    
     public function create()
     {
-        return view('holiday.holidayApplication');
+        return view('holiday.create');
+    }
+    
+    public function store()
+    {
+        return redirect('holiday/holiday_applications/new');
+    }
+    
+    public function duration()
+    {
+        return $this;
+    }
+    
+    //admin共通
+    public function show()
+    {
+        return view('holiday.show');
+    }
+    
+    public function edit()
+    {
+        return view('holiday.edit');
+    }
+    
+    public function update()
+    {
+        return redirect('holiday/holiday_applications/{holidayApplication}/edit');
     }
     
     
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,38 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/new', 'HolidayAppController@create');
-
-
 Auth::routes();
+
+//一覧画面
+Route::get('holiday/holiday_applications', 'HolidayAppController@index')->name('holiday_index');
+//申請画面
+Route::get('holiday/holiday_applications/new', 'HolidayAppController@create')->name('holiday_create');
+//新規作成
+Route::post('holiday/holiday_applications/new', 'HolidayAppController@store')->name('holiday_store');
+//合計期間の算出を行うAjax通信
+Route::get('holiday/get_holiday_duration', 'HolidayAppController@duration')->name('get_holiday_duration');
+//詳細画面
+Route::get('holiday/holiday_applications/{holidayApplication}/show', 'HolidayAppController@show')->name('holiday_show');
+//修正画面
+Route::get('holiday/holiday_applications/{holidayApplication}/edit', 'HolidayAppController@edit')->name('holiday_edit');
+//更新
+Route::post('holiday/holiday_applications/{holidayApplication}/edit', 'HolidayAppController@update')->name('holiday_update');
+
+
+
+//管理者画面
+//一覧画面
+Route::get('holiday/admin/holidayapplications/index', 'HolidayAppController@index')->name('admin_holiday_index');
+//詳細画面
+Route::get('holiday/admin/holidayapplications/{holidayApplication}/show', 'HolidayAppController@store')->name('admin_holiday_store');
+//確定
+Route::put('holiday/admin/holidayapplications/{holidayApplication}/show', 'HolidayAppController@confirm')->name('admin_holiday_confirm');
+//確定取消
+Route::put('holiday/admin/holidayapplications/{holidayApplication}/show', 'HolidayAppController@reject')->name('admin_holiday_reject');
+
+
+
+
+
 
 Route::get('/home', 'HomeController@index')->name('home');
